6771409ce424189e74214004fb49558775d7e7f7
Zeel-1003 <zeel.thakkar@etatvasoft.com>
2024-01-19 18:41:32 +0530

    Request page completion commit

--- js/custom.js
+++ js/custom.js
@@ -17,3 +17,14 @@ function changetheme(){
         document.documentElement.setAttribute('data-bs-theme','light')
     }
 }
+function getFile() {
+    document.getElementById("upfile").click();
+  }
+  
+  function sub(obj) {
+    var file = obj.value;
+    var fileName = file.split("\\");
+    document.getElementById("yourBtn").innerHTML = fileName[fileName.length - 1];
+    document.myForm.submit();
+    event.preventDefault();
+  }
